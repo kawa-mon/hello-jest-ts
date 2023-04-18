bcf40c6ea33dcd4e1c0b8f9eadc2b4acd0be7bf4
kawa-mon <kawashingo1990@gmail.com>
2023-04-18 12:16:14 +0900

    テストを並列実行

---
+++ src/chapter2/group/concurrent.test.ts
@@ -0,0 +1,15 @@
+// 1秒後に`lemon`文字列を返します
+const fetchData = () =>
+  new Promise((resolve) => setTimeout(resolve, 1000, 'lemon'))
+
+// fetchData関数を100回実行するテスト
+// skipを追加
+test.concurrent.skip.each(
+  Array.from(new Array(100).keys()).map((n) => ({
+    n,
+    expected: 'lemon',
+  }))
+)('fetchData $n', async ({ n, expected }) => {
+  console.log(n)
+  await expect(fetchData()).resolves.toBe(expected)
+})
